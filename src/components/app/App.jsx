6a3b14c1a42988091466499c100ab9432e821131
Daria <dbautova@yandex.ru>
2022-06-07 14:30:52 +0300

    routing

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,28 @@
 import WelcomePage from "../welcomePage/WelcomePage";
+//import {  ScrollView } from 'react-native';
+import { Loader } from '@ui5/webcomponents-react';
+import React, { Suspense,
+  //useEffect, 
+//  useState 
+} from 'react';
+import {
+	BrowserRouter,
+	Route,
+	Routes,
+} from 'react-router-dom';
+
 
 function App() {
     return (
-      <div className="App">
-          <WelcomePage/>
-      </div>
+      <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+
+      <Routes>
+        <Route path={'/'} element={<WelcomePage />}/>
+
+      </Routes>
+      </Suspense>
+      </BrowserRouter>
     );
   }
   
